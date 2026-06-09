7ab0a49b6a95e1db548fd2b1c2bb3b80fbaef384
Giliam Breems <giliam.breems@greenchoice.nl>
2026-06-09 12:33:16 +0200

    Add SQLcl login.sql to configure statusbar in footer

---
+++ sqlcl/login.sql
@@ -0,0 +1,15 @@
+set statusbar on
+set statusbar add editmode
+set statusbar add txn
+set statusbar add timing
+set statusbar add encoding
+set statusbar add git
+set statusbar add cwd
+
+set highlighting on
+set highlighting keyword foreground green
+set highlighting identifier foreground magenta
+set highlighting string foreground yellow
+set highlighting number foreground cyan
+set highlighting comment background white
+set highlighting comment foreground black
